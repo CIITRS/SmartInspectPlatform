79f31893749cb0c63f8586a9453f7db2e3f41b5e
CIITRS <86684934+CIITRS@users.noreply.github.com>
2026-08-01 20:31:18 +0800

    fix: correct Baidu V1 signing key derivation

--- huawei-go/internal/appversion/version.go
+++ huawei-go/internal/appversion/version.go
@@ -3,7 +3,7 @@ package appversion
 const GitHubRepository = "CIITRS/SmartInspectPlatform"
 
 var (
-	CurrentVersion = "v0.2.7"
+	CurrentVersion = "v0.2.8"
 	ReleaseDate    = "2026-07-30"
 	BuildCommit    = "development"
 	BuildDate      = ""
